19192b4c2d51f4aa428b55e8ba1a62b00794c86a
muster-Wugui <tintinpost28@gmail.com>
2024-09-25 09:58:40 -0700

    Initial Commit

---
+++ COMSC-210-Code/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  COMSC-210-Code
+//
+//  Created by Xiao Zhang on 9/25/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
