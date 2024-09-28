fd9111d99ecd1bea4735860d73f962c58d7a947a
muster-Wugui <tintinpost28@gmail.com>
2024-09-28 12:41:04 -0700

    Initial Commit

--- COMSC-210-Code/main.cpp
+++ COMSC-210-Code/main.cpp
@@ -1,108 +1,54 @@
-// COMSC-210 | Lab 14 | Xiao Zhang
+// COMSC-210 | Lab 15 | Xiao Zhang
 
 #include <iostream>
-#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
-
-const int MIN = 0;
-const int MAX = 255;
-
-class Color {
+class Movie {
 private:
-    int r, g, b; // red, green, blue
+    string title;
+    int year;
+    string writer;
 
 public:
     // Default constructor
-    Color();
-    
+    Movie() : title(""), year(0), writer("") {
+        
+    }
+
     // Parameterized constructor
-    Color(int red, int green, int blue);
+    Movie(string t, int y, string w) : title(t), year(y), writer(w) {
+        
+    }
 
-    // Getters and setters
-    int getR() const;
-    void setR(int red);
+    // Setters
+    void setTitle(const string& t) {
+        title = t;
+    }
+    void setYear(int y) {
+        year = y;
+    }
+    void setWriter(const string& w) {
+        writer = w;
+    }
 
-    int getG() const;
-    void setG(int green);
+    // Getters
+    string getTitle() const {
+        return title;
+    }
+    int getYear() const {
+        return year;
+    }
+    string getWriter() const {
+        return writer;
+    }
 
-    int getB() const;
-    void setB(int blue);
-
-    // Prints the RGB values
-    void show() const;
+    void show() const {
+        cout << "Movie: " << title << endl;
+        cout << "    Year released: " << year << endl;
+        cout << "    Screenwriter: " << writer << endl;
+    }
 };
-
-void showColors(const Color& c1, const Color& c2, const Color& c3);
-
-int main() {
-    Color redColor(255, 0, 0);
-    Color greenColor(0, 255, 0);
-    Color blueColor(0, 0, 255);
-
-    // Showing the color values
-    showColors(redColor, greenColor, blueColor);
-
-    return 0;
-}
-
-
-// Default constructor
-Color::Color() : r(0), g(0), b(0) {}
-
-// Parameterized constructor
-Color::Color(int red, int green, int blue) {
-    setR(red);
-    setG(green);
-    setB(blue);
-}
-
-// Getter and Setter for Red
-int Color::getR() const {
-    return r;
-}
-
-void Color::setR(int red) {
-    if (red >= MIN && red <= MAX)
-        r = red;
-    else
-        cerr << "Error: " << red << "\n";
-}
-
-// Getter and Setter for Green
-int Color::getG() const {
-    return g;
-}
-
-void Color::setG(int green) {
-    if (green >= MIN && green <= MAX)
-        g = green;
-    else
-        cerr << "Error: " << green << "\n";
-}
-
-// Getter and Setter for Blue
-int Color::getB() const {
-    return b;
-}
-
-void Color::setB(int blue) {
-    if (blue >= MIN && blue <= MAX)
-        b = blue;
-    else
-        cerr << "Error: " << blue << "\n";
-}
-
-// Prints the RGB values
-void Color::show() const {
-    cout << "Color: (" << setw(3) << r << ", " << setw(3) << g << ", " << setw(3) << b << ")\n"; //The setw function helps to control the formatting of output(learned from internet)
-}
-
-// Prints out a few Color objects
-void showColors(const Color& c1, const Color& c2, const Color& c3) {
-    cout << "Here are the colors:\n";
-    c1.show();
-    c2.show();
-    c3.show();
-}
