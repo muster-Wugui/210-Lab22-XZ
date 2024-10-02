b45d0bc26c24eecc33e661913135cc5ac68b712e
muster-Wugui <tintinpost28@gmail.com>
2024-10-02 11:38:34 -0700

    Initial Commit

--- COMSC-210-Code/main.cpp
+++ COMSC-210-Code/main.cpp
@@ -1,84 +1,123 @@
-// COMSC-210 | Lab 15 | Xiao Zhang
+// COMSC-210 | Lab 16 | Xiao Zhang
 
 #include <iostream>
-#include <fstream>
-#include <vector>
-#include <string>
+#include <iomanip>
 
 using namespace std;
 
-class Movie {
+const int MIN = 0;
+const int MAX = 255;
+
+class Color {
 private:
-    string title;
-    int year;
-    string writer;
+    int r, g, b; // red, green, blue
 
 public:
     // Default constructor
-    Movie() : title(""), year(0), writer("") {
-        
-    }
+    Color();
 
     // Parameterized constructor
-    Movie(string t, int y, string w) : title(t), year(y), writer(w) {
-        
-    }
+    Color(int red, int green, int blue);
 
-    // Setters
-    void setTitle(const string& t) {
-        title = t;
-    }
-    void setYear(int y) {
-        year = y;
-    }
-    void setWriter(const string& w) {
-        writer = w;
-    }
+    // Partial constructor (default green and blue to 0)
+    Color(int red);
 
-    // Getters
-    string getTitle() const {
-        return title;
-    }
-    int getYear() const {
-        return year;
-    }
-    string getWriter() const {
-        return writer;
-    }
+    int getR() const;
+    
+    void setR(int red);
 
-    void show() const {
-        cout << "Movie: " << title << endl;
-        cout << "    Year released: " << year << endl;
-        cout << "    Screenwriter: " << writer << endl;
-    }
+    int getG() const;
+    
+    void setG(int green);
+
+    int getB() const;
+    
+    void setB(int blue);
+
+    void show() const;
 };
 
+void showColors(const Color& c1, const Color& c2, const Color& c3);
+
 int main() {
-    vector<Movie> moviesList; // I chose to use vector as container
-    ifstream file("input.txt");
-    string title, writer;
-    int year;
+    // Using the parameterized constructor
+    Color redC(255, 0, 0);
+    
+    // Using the default constructor
+    Color defaultC;
 
-    if (!file) {
-        cout << "Can't open the file" << endl;
-        return 0;
-    }
+    // Using the partial constructor
+    Color partialR(128);
 
-    while (getline(file, title)) {
-        file >> year;
-        file.ignore(); // This will ignore newline after reading year
-        getline(file, writer);
+    // Using the parameterized constructor for custom the color
+    Color custom(50, 100, 150);
 
-        // add movie to the vector
-        moviesList.push_back(Movie(title, year, writer));
-    }
+    // Outputing the color values
+    showColors(redC, defaultC, partialR);
 
-    file.close();
-
-    for (const Movie& movie : moviesList) {
-        movie.show();
-        cout<<endl;
-    }
+    cout << "Custom Color:"<<endl;
+    custom.show();
 
     return 0;
+}
+
+// default constructor
+Color::Color() : r(0), g(0), b(0) {}
+
+// Parameterized constructor
+Color::Color(int red, int green, int blue) {
+    setR(red);
+    setG(green);
+    setB(blue);
+}
+
+// Partial constructor, initializing only red
+Color::Color(int red) : r(0), g(0), b(0) {
+    setR(red);
+}
+
+int Color::getR() const {
+    return r;
+}
+
+void Color::setR(int red) {
+    if (red >= MIN && red <= MAX)
+        r = red;
+    else
+        cerr << "Error: " << red << "\n";
+}
+
+int Color::getG() const {
+    return g;
+}
+
+void Color::setG(int green) {
+    if (green >= MIN && green <= MAX)
+        g = green;
+    else
+        cerr << "Error: " << green << "\n";
+}
+
+int Color::getB() const {
+    return b;
+}
+
+void Color::setB(int blue) {
+    if (blue >= MIN && blue <= MAX)
+        b = blue;
+    else
+        cerr << "Error: " << blue << "\n";
+}
+
+// Prints the RGB values
+void Color::show() const {
+    cout << "Color: (" << setw(3) << r << ", " << setw(3) << g << ", " << setw(3) << b << ")\n";
+}
+
+// Prints out a few Color objects
+void showColors(const Color& c1, const Color& c2, const Color& c3) {
+    cout << "Here are the colors:\n";
+    c1.show();
+    c2.show();
+    c3.show();
 }
