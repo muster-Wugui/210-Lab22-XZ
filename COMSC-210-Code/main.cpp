71619ad97bdbc2c367be1b463a4fca61ef79f3fd
muster-Wugui <tintinpost28@gmail.com>
2024-10-05 18:25:49 -0700

    Initial Commit

--- COMSC-210-Code/main.cpp
+++ COMSC-210-Code/main.cpp
@@ -1,123 +1,87 @@
-// COMSC-210 | Lab 18 | Xiao Zhang
+// COMSC-210 | Lab 19 | Xiao Zhang
 
 #include <iostream>
-#include <iomanip>
 #include <string>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
 using namespace std;
 
-// Review struct to store rating and comments
 struct Review {
-    double rating; // Movie rating (0.0 - 5.0)
-    string text; // Review text
-    Review* next; // Pointer to next review
+    float score;
+    string note;
+    Review* next;
+    Review(float s, const string& n) : score(s), note(n), next(nullptr) {} //constructor
 };
 
-void addToHead(Review*& head, double rating, string& text);
-void addToTail(Review*& head, double rating, string& text);
-void showAll(Review* head);
-double getAvg(Review* head);
+// Movie class to hold movie details and reviews
+class Movie {
+public:
+    string name;
+    Review* reviews;
+    Movie(const string& movieName) : name(movieName), reviews(nullptr) {}
+    //destructor
+    ~Movie() {
+        while (reviews != nullptr) {
+            Review* temp = reviews;
+            reviews = reviews->next;
+            delete temp;
+        }
+    }
 
+    // Add review at the start of the list
+    void addReview(float score, const string& note) {
+        Review* newReview = new Review(score, note);
+        newReview->next = reviews;
+        reviews = newReview;
+    }
+
+    // Show all reviews
+    void showReviews() {
+        cout << "Reviews for: " << name << endl;
+        Review* temp = reviews;
+        while (temp != nullptr) {
+            cout << "  - " << temp->score << ": " << temp->note << endl;
+            temp = temp->next;
+        }
+    }
+};
+
+float randrating();
+void defaltR(Movie& movie);
 int main() {
-    Review* head;
-    int option;
-    double rating;
-    string text;
-    char more;
+    srand(static_cast<unsigned int>(time(0)));  // Seed for random numbers
 
-    // Pick where to add new nodes
-    cout << "Which method to add reviews?\n";
-    cout << "    [1] Add to head\n";
-    cout << "    [2] Add to tail\n";
-    cout << "Choice: ";
-    cin >> option;
+    Movie movie1("The Matrix");
+    Movie movie2("Inception");
 
+    // If no files, add some default reviews
+    defaltR(movie1);
+    defaltR(movie2);
 
-    do {
-        // Get review details
-        cout << "Enter rating (0-5): ";
-        cin >> rating;
-
-        // Validate rating input
-        while (rating < 0.0 || rating > 5.0) {
-            cout << "Invalid. Only between 0 to 5: ";
-            cin >> rating;
-        }
-
-        cin.ignore(); // Skip leftover newline, I learned this online again
-        cout << "Enter comments: ";
-        getline(cin, text);
-
-        // Add review based on choice
-        if (option == 1) {
-            addToHead(head, rating, text);
-        } else {
-            addToTail(head, rating, text);
-        }
-
-        cout << "Add another review? Y/N: ";
-        cin >> more;
-
-    } while (more == 'Y' || more == 'y');
-
-    // Show reviews and calculate average
-    cout << "\nReviews:\n";
-    showAll(head);
-
-    double avg = getAvg(head);
-    cout << "> Average: " << avg << endl;
+    movie1.showReviews();
+    movie2.showReviews();
 
     return 0;
 }
 
-// Adds new review at head
-void addToHead(Review*& head, double rating, string& text) {
-    Review* newReview = new Review;
-    newReview->rating = rating;
-    newReview->text = text;
-    newReview->next = head; // Point to old head
-    head = newReview; // Set as new head
+// Get random score between 1.0 and 5.0
+float randrating() {
+    return static_cast<float>(rand() % 41 + 10) / 10.0;
 }
 
-// Adds new review at tail
-void addToTail(Review*& head, double rating, string& text) {
-    Review* newReview = new Review; // Allocate new node
-    newReview->rating = rating;
-    newReview->text = text;
-    newReview->next = nullptr;
-
-    if (head == nullptr) {
-        head = newReview;
-    } else {
-        Review* temp = head;
-        while (temp->next != nullptr) {
-            temp = temp->next;
-        }
-        temp->next = newReview;
+// Add some default reviews if I don't have files
+void defaltR(Movie& movie) {
+    string Dcomments[] = {
+        "Great storyline!",
+        "Loved the characters.",
+        "Amazing visual.",
+        "want to watch it again!",
+    };
+    for (int i = 0; i < 3; ++i) {  // Add three reviews randomly
+        float score = randrating();
+        movie.addReview(score, Dcomments[i % 5]);
     }
 }
 
-// Show all reviews
-void showAll(Review* head) {
-    int num = 1;
-    Review* temp = head;
-    while (temp != nullptr) {
-        cout << "    > Review #" << num << ": " << temp->rating << ": " << temp->text << endl;
-        temp = temp->next;
-        num++;
-    }
-}
 
-// Get average rating
-double getAvg(Review* head) {
-    int count = 0;
-    double total = 0.0;
-    Review* temp = head;
-
-    // Calculate total and count
-    while (temp != nullptr) {
-        total += temp->rating;
-        count++;
-        temp = temp->next;
-    }
-        return total / count;
-    }
